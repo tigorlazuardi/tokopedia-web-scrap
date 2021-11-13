02a25bbb6c6082b30cff6a7fe089bc0039cbc0ba
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-11-13 11:26:32 +0700

    doc(csvwriter-Error): added docs to tell why the methods are implemented

--- csvwriter/error.go
+++ csvwriter/error.go
@@ -19,6 +19,7 @@ func wrapError(cause error, msgFormat string, args ...interface{}) Error {
 	return Error{fmt.Sprintf(msgFormat, args...), cause, "csvwriter", trace}
 }
 
+// implementes error interface
 func (e Error) Error() string {
 	if e.cause != nil {
 		return e.message + ": " + e.cause.Error()
@@ -26,10 +27,13 @@ func (e Error) Error() string {
 	return e.message + ": " + "<nil>"
 }
 
+// implements the silent errors.Unwrap interface
 func (e Error) Unwrap() error {
 	return e.cause
 }
 
+// implements json.Marshaler interface.
+// because we want to know what the actual error is even after marshaled.
 func (e Error) MarhsalJSON() ([]byte, error) {
 	m := map[string]interface{}{
 		"message":  e.message,
