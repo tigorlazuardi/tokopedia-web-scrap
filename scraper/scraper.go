fe2f57e0f60251a34b930765a8981fe04c2f9b88
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2021-11-13 15:41:17 +0700

    feat(scraper): added implementation to writing csv

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -1,12 +1,47 @@
 package scraper
 
-import "io"
+import "strconv"
 
 type Scraper interface {
 	// The filter to check if http destination should be scraped
 	Filter(string) bool
 	// TODO: Change to HTML Element later
 	Parse(string /* should be html element */) error
-	// Gets the parsed data stream
-	Stream() io.ReadCloser
+}
+
+type ScrapeData struct {
+	ProductName  string `json:"product_name"`
+	Description  string `json:"description"`
+	ImageLink    string `json:"image_link"`
+	Price        int    `json:"price"`
+	Rating       int    `json:"rating"`
+	MerchantName string `json:"merchant_name"`
+}
+
+func (s ScrapeData) CSVHeader() []string {
+	return []string{
+		"product_name", "description", "image_link",
+		"price", "rating", "merchant_name",
+	}
+}
+
+func (s ScrapeData) CSVRow() []string {
+	return []string{
+		s.ProductName, s.Description, s.ImageLink,
+		strconv.Itoa(s.Price), strconv.Itoa(s.Rating), s.MerchantName,
+	}
+}
+
+type ScrapeDataList []ScrapeData
+
+func (sdl ScrapeDataList) CSVHeader() []string {
+	return (ScrapeData{}).CSVHeader()
+}
+
+func (sdl ScrapeDataList) CSVRows() [][]string {
+	list := make([][]string, len(sdl))
+	for _, scrapeData := range sdl {
+		list = append(list, scrapeData.CSVRow())
+	}
+	return list
 }
